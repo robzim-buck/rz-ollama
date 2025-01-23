a1eb481d5dddae57c49d25b0ee8f370a986c1a19
Rob Zimmelman <rob.zimmelman@buck.co>
2025-01-23 08:37:57 -0500

    buck zendesk bot in span

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
-from rz_chat import get_rag_chain_for_pdf, ask_question, get_rag_chain_for_json, get_retriever_with_history, get_vector_db_retriever_for_json, \
+from rz_chat import get_rag_chain_for_pdf, ask_question, get_rag_chain_for_json, \
+    get_retriever_with_history, get_vector_db_retriever_for_json, \
     openai_llm, get_vector_db_retriever_for_pdf
+import json2html
+
+
 
 my_json_retriever_with_history = get_retriever_with_history(retriever=get_vector_db_retriever_for_json(), llm=openai_llm)
-my_pdf_retriever_with_history = get_retriever_with_history(retriever=get_vector_db_retriever_for_pdf(), llm=openai_llm)
+# my_pdf_retriever_with_history = get_retriever_with_history(retriever=get_vector_db_retriever_for_pdf(), llm=openai_llm)
 
-pdf_chain = get_rag_chain_for_pdf(retriever_with_history=my_pdf_retriever_with_history)
+# pdf_chain = get_rag_chain_for_pdf(retriever_with_history=my_pdf_retriever_with_history)
 json_chain = get_rag_chain_for_json(retriever_with_history=my_json_retriever_with_history)
 
 app = Flask(__name__)
@@ -15,16 +19,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/linux_python_chat', methods=['GET', 'POST'])
-def linux_python():
-    message = request.form['msg']
-    return ask_question(rag_chain=pdf_chain, question=message)
+# @app.route('/linux_python_chat', methods=['GET', 'POST'])
+# def linux_python():
+#     message = request.form['msg']
+#     return ask_question(rag_chain=pdf_chain, question=message)
 
 
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     message = request.form['msg']
-    return ask_question(rag_chain=json_chain, question=message)
+    result = ask_question(rag_chain=json_chain, question=message)
+    return json2html.json2html.convert(json = result)
+    # return result
 
 
 if __name__ == '__main__':
